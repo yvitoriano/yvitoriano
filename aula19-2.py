5335c68a209d4d597b35b38c15c00a05e9d70c70
Yasmin da Silva Vitoriano Medeiros <20231041110021@ifrn.local>
2024-11-19 16:07:09 -0300

    Aula 19/11/2024

---
+++ aula19-2.py
@@ -0,0 +1,59 @@
+# Funções de Strings 
+# String methods - site Python
+#Estruturas de cadeias de caracteres
+# 0 1 2 3 4
+
+#nome1 = 'Yasmin'
+
+#print(nome1.center(30,'a'))
+
+#Funções
+
+#def funcsoma(n1, n2):
+#    soma = n1 + n2
+#    print (soma)
+ #   return soma
+
+#valor_soma = funcsoma(10,20)
+
+#print(valor_soma)
+
+#def nome_completo(nome1, nome2):
+#    junto = nome1 + " " + nome2
+ #   return junto
+
+#print(nome_completo('Yasmin', 'Silva'))
+
+
+#def pode_entrar(genero, idade, nome):
+ #  nome_invalido_fem = nome[0].lower() in ('l', 'm', 'n')
+
+  #  if genero == "masculino" and idade > 18 and not nome_invalido_masc:
+    #    print("Pode entrar")
+   # elif genero == "feminino" and idade > 17 and not nome_invalido_fem:
+       # print("Pode entrar")
+   # else:
+     #   print("Não pode entrar")  
+
+# Exemplos de chamadas
+#pessoa1 = #pode_entrar("masculino", 22, "Carlos")  
+#pessoa2 = #pode_entrar("feminino", 20, "Larissa")  
+#pessoa3 = #pode_entrar("feminino", 18, "Julia")    
+#pessoa4 = #pode_entrar("masculino", 17, "Carlos")  
+#pessoa5 = #pode_entrar("feminino", 20, "Maria")    
+
+
+#----------------------------------------------------------
+
+#declaração globa e local 
+#Recursividade
+#fib
+
+
+
+
+       
+
+    
+
+
---
+++ aula19.py
@@ -0,0 +1,41 @@
+# Chaves(Dicionário) e valor 
+
+# JSON #API
+#pessoa = {"nome" : " ",
+#           "idade": 28,
+#           "email" : "email@email.com",
+ #           "endereço": "Rua lalala"
+#}
+
+#pessoa['nome'] = {'Yasmin','da','Silva'}
+
+# print(pessoa['nome'][2])
+
+
+Marido = { "id": 98887765,
+           "nome":{
+               "p_nome": "Tiago",
+               "u_nome": "Barreto",
+           },
+           "idade": 28,
+           "email": "email@marido.com",
+           'senha' : 12345,
+           "endereço" : "Rua lalala",
+}
+
+Esposa = { "id": 76800004,
+           "nome":{
+               "p_nome": "Maria",
+               "u_nome": "Silva",
+           },
+           "idade": 26,
+           "email": "email@esposa.com",
+           'senha' : 342644,
+           "endereço" : "Rua lalele",
+}
+
+Esposa['nome'] ['u_nome'] = Esposa['nome'] [u_nome] + " " + Marido ['nome'] ['u_nome']
+
+print(Esposa ['nome'])
+
+
