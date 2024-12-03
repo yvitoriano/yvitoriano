3c2f441d88e3b1041dceb4494b29f296e36e7e23
Yasmin da Silva Vitoriano Medeiros <20231041110021@ifrn.local>
2024-12-03 15:41:52 -0300

    Herança

--- teste_poo.py
+++ teste_poo.py
@@ -1,14 +1,54 @@
-class Aluno:
-    def __init__(self,nome,matricula,idade,cpf):
-        self.nome = nome
-        self.__matricula = matricula
-        self.idade = idade
-        self.__cpf = cpf 
+class Animal():
+    def __init__(self, nome, peso):
+        self.__nome = nome 
+        self.__peso = peso
 
-    def get_matricula(self):
-        return self.__matricula
+    def __str__(self):
+        return 'Nome %s \n Peso: %f'%(self.__nome, self.__peso)
     
-    def get_cpf(self):
-        return self.__cpf
+    def alimentar(self, comida):
+        self.peso += comida
     
-    def set_matricula(self, )
+    #maior que 
+    def __gt__(self, other):
+        return self.__peso > other.__peso
+    
+    def __add__(self, other):
+        return Animal(self.__nome + ' ' + other.__nome, self.__peso + other.__peso)
+    @property
+    def nome(self):
+        print('Getter foi chamado')
+        return self.nome 
+    
+    @nome.setter
+    def nome (self, new_name):
+        print('Setter foi chamado')
+        if type(new_name) == type(str()):
+            self.__nome = new_name
+        else:
+            print('O nome deve ser uma string')
+
+#Leao = Animal('Leão',200)
+#Tigre = Animal('Tigre', 150)
+#animals = Leao + Tigre
+#print(animals)
+
+#Sobrecarga de operadores 
+            
+class Mamímefero(Animal):
+    classe = 'Mamalia'
+
+    def __str__(self):
+        return super().__str__() + '\nMamífero\n'
+    
+class Ave(Animal):
+    def __str__(self):
+        return super().__str__() + "\nAve\n"
+    
+class Ave_Pequena(Ave):
+    pass
+
+Leao = Mamífero('Leao', 200)
+Pica_pau = Ave("Pica pau" , 2)
+print(Pica_pau)
+
